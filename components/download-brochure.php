80437842cd3b42d2c5bfa47931da4e840f9fdcdd
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2024-11-13 09:21:20 +0000

    added media directory and brochure logic.

--- components/download-brochure.php
+++ components/download-brochure.php
@@ -5,9 +5,15 @@
                 <strong>PONOS</strong> is a fully integrated engineering and construction firm with a remarkable track record of successful projects. The company is known for its expertise in construction works and offers top-tier project management services across the engineering spectrum.
             </p>
             <div class="brochure-cta">
-                <a href="/wp-content/uploads/2024/10/PONOS-SUPPORT-SERVICES-LIMITED-1.pdf" target="_blank" class="ponos-cta">
+            <?php if ( $ponos_brochure_file_link = get_field( 'ponos_brochure_file_link' ) ) : ?>
+                <a href="<?php echo esc_html( $ponos_brochure_file_link ); ?>" target="_blank" class="ponos-cta">
                     <i class="fa-solid fa-download"></i>Download Brochure
                 </a>
+            <?php else : ?>
+                <a href="<?php echo get_template_directory_uri() . "/media/PONOS-NEW-BRONCHOR.pdf" ?>" target="_blank" class="ponos-cta">
+                    <i class="fa-solid fa-download"></i>Download Brochure
+                </a>
+            <?php endif; ?>
             </div>
         </div>
     </div>
